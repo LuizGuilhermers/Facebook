c9c948e2b137b5a534c07b3f4d4be6b4e3af2b48
LuizGuilhermers <132360055+LuizGuilhermers@users.noreply.github.com>
2024-05-07 16:19:07 -0300

    Facebook

---
+++ src/css/botaoStyle.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    button: {
+     paddingVertical: 15,
+     paddingHorizontal: 135,
+     borderRadius: 5,
+     marginVertical: 10,
+    },
+    buttonPressed: {
+        backgroundColor: '#0056b3'
+    },
+    buttonText: {
+        fontSize: 22,
+        textAlign: 'center',
+    }
+  });
+  
